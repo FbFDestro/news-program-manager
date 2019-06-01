847135e35ed61c734852e3272668479e98371177
FbFDestro <fbfdestro@gmail.com>
2019-06-01 14:09:22 -0300

    bd setup code

--- server/index.js
+++ server/index.js
@@ -5,11 +5,16 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+const bdSetup = require('./bdsetup');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/pessoas', pessoas);
+
+bdSetup.drop();
+bdSetup.create();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- server/routes/pessoas.js
+++ server/routes/pessoas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const conexao = require('../conexao')
+const conexao = require('../conexao');
 
 router.use(express.json()) // for parsing application/json
 router.use(express.urlencoded({
