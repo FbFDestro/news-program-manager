597a3a6da3e2c42684f6d1c6bab3c3dc31f6b35e
paulocarneiro99 <pandrecarneiro@gmail.com>
2019-06-13 20:22:35 -0300

    retornando nome dos jornalistas

--- server/routes/materias.js
+++ server/routes/materias.js
@@ -10,7 +10,10 @@ router.use(express.urlencoded({
 
 router.get('/', async (request, response) => { // usando await async
     try {
-        const sql = `SELECT * FROM MATERIA`;
+        const sql = `SELECT * FROM MATERIA M
+                        join PESSOA P 
+                            ON P.CPF = M.jornalista;            
+        `;
 
         console.log(sql);
 
@@ -40,6 +43,24 @@ router.get('/quantidade/:mes', async (request, response) => { // usando await as
     }
 });
 
+
+router.get('/:cpf', async (request, response) => { // usando await async
+    try {
+        const sql = `SELECT * FROM MATERIA M
+                        join PESSOA P 
+                            ON P.CPF = M.jornalista
+                    where M.jornalista = ${request.params.cpf};          
+        `;
+
+        console.log(sql);
+
+        const results = await conexao.query(sql);
+        response.status(200).json(results.rows);
+    } catch (err) {
+        response.status(404).send("Not found");
+        console.log('Database ' + err);
+    }
+});
 
 router.get('/jornalistaspormes', async (request, response) => { // usando await async
     try {
