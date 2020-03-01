c712610871da8a5beaf1c2bbcb7bc2c2c865e681
FbFDestro <fbfdestro@gmail.com>
2020-03-01 19:37:42 -0300

    added proxy and updated dependecies

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => {
+        this.setState({ data: res });
+      })
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/api/pessoas/');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. Test
+          </p>
+          <p>{this.state.data && this.state.data.length}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
