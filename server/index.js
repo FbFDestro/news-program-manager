738588140618e981bfc249868e4487ed2091fd26
FbFDestro <fbfdestro@gmail.com>
2020-04-12 18:44:10 -0300

    removed vanilla js client (react.js now available)

--- server/index.js
+++ server/index.js
@@ -14,8 +14,6 @@ var path = require('path');
 
 const bdSetup = require('./bdsetup');
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 app.use('/api/pessoas', pessoas);
 app.use('/api/pautas', pautas);
